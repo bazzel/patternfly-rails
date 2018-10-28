78d5713628f0b06b2987f6d0d3b7bbcde715fd9e
Patrick Baselier <patrickbaselier@Patricks-MacBook-Pro.local>
2018-10-28 09:30:14 +0100

    Refactor

--- app/helpers/pattern_fly/alert.rb
+++ app/helpers/pattern_fly/alert.rb
@@ -1,6 +1,6 @@
 module PatternFly
   class Alert
-    ALERT_TYPES = [:success, :info, :warning, :danger] unless const_defined?(:ALERT_TYPES)
+    PF_ALERT_TYPES = [:success, :info, :warning, :danger]
     delegate :flash, :raw, :content_tag, :safe_join, to: :@template
 
     ICON_CLASSES = {
@@ -23,24 +23,15 @@ module PatternFly
       flash.each do |type, message|
         # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
         next if message.blank?
-
-        type = type.to_sym
-        type = :success if type == :notice
-        type = :danger  if type == :alert
-        type = :danger  if type == :error
-        next unless ALERT_TYPES.include?(type)
-
-        icon_class = ICON_CLASSES[type]
-
-        icon = content_tag(:span, '', class: "pficon #{icon_class}")
+        next unless pf_type = pf_type(type)
 
         tag_class = options.extract!(:class)[:class]
         tag_options = {
-          class: "alert fade in alert-#{type} #{tag_class}"
+          class: "alert fade in alert-#{pf_type} #{tag_class}"
         }.merge(options)
 
         Array(message).each do |msg|
-          text = content_tag(:div, close_button + icon + msg, tag_options)
+          text = content_tag(:div, close_button + icon(pf_type) + msg, tag_options)
           flash_messages << text if msg
         end
       end
@@ -50,9 +41,27 @@ module PatternFly
     private
 
     def close_button
-      content_tag(:button, type: 'button', class: 'close', 'data-dismiss' => 'alert') do
-        content_tag(:span, '', class: 'pficon pficon-close')
-      end
+      @close_button ||= begin
+                          content_tag(:button, type: 'button', class: 'close', 'data-dismiss' => 'alert') do
+                            content_tag(:span, '', class: 'pficon pficon-close')
+                          end
+                        end
+    end
+
+    def icon(type)
+      icon_class = ICON_CLASSES[type]
+
+      content_tag(:span, '', class: "pficon #{icon_class}")
+    end
+
+    def pf_type(type)
+      pf_type = case type.to_sym
+                when :notice then :success
+                when :alert, :error then :danger
+                else type.to_sym
+                end
+
+      PF_ALERT_TYPES.include?(pf_type) ? pf_type : nil
     end
   end
 end
