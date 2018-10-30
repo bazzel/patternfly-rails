92c9d48a78b65a768ece490750e6716465f0280c
Patrick Baselier <patrickbaselier@patricks-mbp.eu.asml.com>
2018-10-30 06:52:05 +0100

    Override Loader Options

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -13,4 +13,18 @@ environment.plugins.prepend(
   })
 )
 
+const merge = require('webpack-merge')
+
+const myCssLoaderOptions = {
+  modules: true,
+  sourceMap: true,
+  localIdentName: '[name]__[local]___[hash:base64:5]'
+}
+
+const CSSLoader = environment.loaders.get('sass').use.find(el => el.loader === 'css-loader')
+
+CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions)
+
 module.exports = environment
+
+
